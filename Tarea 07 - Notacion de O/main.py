759ca6326e4a31439d5182e12a23b87774e3901b
Christian_Windows_Desktop <chgeovany.15@gmail.com>
2022-10-04 00:44:54 -0500

    contador en segundos

--- Tarea 07 - Notacion de O/main.py
+++ Tarea 07 - Notacion de O/main.py
@@ -1,9 +1,13 @@
 if __name__ == "__main__":
     segundos = 0  
     minutos = 0 
-    horas = 0  
+    horas = 0
+    contador = 0
 
     for h in range(24): 
         for m in range(60): 
             for s in range(60): 
                 print(f"{h}:{m}:{s}")
+                contador += 1
+
+    print(f"Se han mostrado {contador} segundos")
